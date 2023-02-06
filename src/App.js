c17c4193a946ec3106f0bf97be4a908aefaf59f1
h-elliot <hunter.e.canavan@gmail.com>
2023-02-06 16:22:34 -0500

    setup basics in App.js and index.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+// import './images/Downtown_Corsicana_Panorama_Wiki_(1_of_1).jpg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h1 class='App-header-text' id='App-site-title'>
+					Beard Mediation, LLC.
+				</h1>
+			</header>
+			<main className='App-main'></main>
+		</div>
+	);
 }
 
 export default App;
