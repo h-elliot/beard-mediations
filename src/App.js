c44e9e197357805a663bb27d73118be64482051f
h-elliot <hunter.e.canavan@gmail.com>
2023-02-08 15:00:31 -0500

    add empty useSticky hook

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import PageContent from './components/PageContent';
 import Footer from './components/Footer';
 import HeroImg from './components/HeroImg';
 import Sidebar from './components/Sidebar';
+import useSticky from './hooks/useSticky';
 
 function App() {
+	const { sticky, stickyRef } = useSticky();
 	return (
 		<div className='App'>
 			<Navbar />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Navbar.css';
+import useSticky from '../hooks/useSticky';
 
 export default function Navbar() {
+	const { sticky, stickyRef } = useSticky();
 	return (
 		<header className='App-header'>
-			<h3 className='App-header-text' id='App-site-title'>
+			<h3 className='Header-text' id='Header-title'>
 				Beard Mediation, LLC.
 			</h3>
-			<ul className='App-header-nav'>
+			<ul className='Header-nav'>
 				<li>About</li>
 				<li>Contact</li>
 			</ul>
