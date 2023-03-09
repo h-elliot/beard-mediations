87152ddf197359278ff086d7d30f14eabf15776d
h-elliot <hunter.e.canavan@gmail.com>
2023-03-09 17:42:03 -0500

    add copy

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -7,62 +7,22 @@ export default function PageContent() {
 		<main className='Page-content-all'>
 			<article className='Page-content-article'>
 				<p className='Page-content-text'>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis
-					massa sed elementum tempus egestas. Ut diam quam nulla porttitor
-					massa. Feugiat in ante metus dictum at tempor commodo. Urna nunc id
-					cursus metus. Et pharetra pharetra massa massa ultricies mi quis
-					hendrerit. Tristique et egestas quis ipsum suspendisse ultrices
-					gravida. Et egestas quis ipsum suspendisse ultrices gravida dictum.
-					Odio euismod lacinia at quis. Pharetra pharetra massa massa ultricies
-					mi quis hendrerit dolor. Egestas dui id ornare arcu. Ornare arcu dui
-					vivamus arcu. Dui id ornare arcu odio ut sem nulla. Adipiscing elit
-					pellentesque habitant morbi. Nunc congue nisi vitae suscipit tellus
-					mauris a. Justo eget magna fermentum iaculis. Tempor nec feugiat nisl
-					pretium. Aenean vel elit scelerisque mauris pellentesque pulvinar
-					pellentesque. Sed vulputate mi sit amet mauris commodo quis imperdiet
-					massa. Ipsum dolor sit amet consectetur adipiscing elit pellentesque
-					habitant morbi. Venenatis urna cursus eget nunc scelerisque viverra
-					mauris in aliquam. Est pellentesque elit ullamcorper dignissim cras
-					tincidunt. Elit scelerisque mauris pellentesque pulvinar pellentesque.
-					Purus in mollis nunc sed id semper risus in. Cursus mattis molestie a
-					iaculis at erat pellentesque. Egestas dui id ornare arcu odio ut.
-					Fringilla phasellus faucibus scelerisque eleifend. Suspendisse sed
-					nisi lacus sed viverra. Praesent elementum facilisis leo vel fringilla
-					est ullamcorper eget nulla. Consequat mauris nunc congue nisi vitae
-					suscipit tellus mauris. Tincidunt dui ut ornare lectus sit amet est
-					placerat. Volutpat odio facilisis mauris sit amet. Duis at consectetur
-					lorem donec massa sapien. Neque ornare aenean euismod elementum nisi
-					quis. Justo laoreet sit amet cursus sit amet dictum sit amet.
-					Ridiculus mus mauris vitae ultricies. Mi in nulla posuere sollicitudin
-					aliquam ultrices. Sit amet mauris commodo quis imperdiet massa
-					tincidunt nunc pulvinar. Mattis molestie a iaculis at erat
-					pellentesque adipiscing. Venenatis cras sed felis eget velit. Quis
-					imperdiet massa tincidunt nunc pulvinar. Ipsum faucibus vitae aliquet
-					nec ullamcorper sit amet. Tristique magna sit amet purus gravida quis
-					blandit turpis cursus. Ipsum suspendisse ultrices gravida dictum fusce
-					ut placerat orci. Sit amet massa vitae tortor. Tristique senectus et
-					netus et malesuada fames. Egestas pretium aenean pharetra magna. Felis
-					eget nunc lobortis mattis aliquam faucibus purus in massa. Sit amet
-					purus gravida quis. Senectus et netus et malesuada fames ac turpis
-					egestas. Convallis posuere morbi leo urna molestie at. Eros donec ac
-					odio tempor orci dapibus ultrices in. Faucibus nisl tincidunt eget
-					nullam non nisi est sit. Arcu felis bibendum ut tristique et egestas
-					quis ipsum. Nunc sed id semper risus in hendrerit gravida rutrum
-					quisque. Sit amet consectetur adipiscing elit duis tristique
-					sollicitudin. Quis varius quam quisque id diam. Malesuada bibendum
-					arcu vitae elementum curabitur. Quis commodo odio aenean sed
-					adipiscing diam donec adipiscing tristique. Nibh nisl condimentum id
-					venenatis a. Mattis aliquam faucibus purus in massa tempor. Aliquam
-					eleifend mi in nulla. Platea dictumst vestibulum rhoncus est
-					pellentesque elit ullamcorper dignissim. Tristique senectus et netus
-					et. Malesuada bibendum arcu vitae elementum curabitur vitae. Consequat
-					mauris nunc congue nisi vitae suscipit tellus mauris. Vulputate
-					dignissim suspendisse in est ante in nibh mauris. Fames ac turpis
-					egestas integer eget aliquet. Nullam ac tortor vitae purus faucibus.
-					Pellentesque habitant morbi tristique senectus. Sed egestas egestas
-					fringilla phasellus faucibus scelerisque eleifend donec. Laoreet id
-					donec ultrices tincidunt arcu non sodales neque.
+					I have been a civil trial attorney specializing in eminent domain
+					litigation for over 30 years, joining the Dawson & Sodd law firm right
+					out of Baylor Law School in 1989. Litigation has always been my focus.
+					I participated as part of the acclaimed Baylor Mock Trial Competition
+					Team and was honored to receive the Matt Dawson Award for Outstanding
+					Trial Advocacy at Baylor. I “hit the ground running” and have been a
+					shareholder at Dawson & Sodd for most of my legal career. I firmly
+					believe my extensive trial and mediation experience handling hundreds
+					of eminent domain cases over the past three decades uniquely qualifies
+					me as a mediator specializing in eminent domain. Only an eminent
+					domain lawyer can understand the particular legal issues involved, the
+					nuances of eminent domain appraisals, and the motivations of both
+					sides involved in a condemnation dispute. My philosophy as a trial
+					lawyer has always been that understanding both sides of a case is
+					absolutely essential to a successful outcome – and it is that same
+					philosophy I will bring to the table as a mediator.
 				</p>
 			</article>
 		</main>
