5005e7ed73f64453ee130699bb1c9ca5a683cd2d
h-elliot <hunter.e.canavan@gmail.com>
2023-03-14 16:24:14 -0400

    clean up unused code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import './Navbar.css';
-// import useSticky from '../hooks/useSticky';
 
 export default function Navbar() {
-	// const { sticky, stickyRef } = useSticky();
 	return (
 		<header className='App-header'>
 			<h3 className='Header-text' id='Header-title'>
 				Beard Mediation, LLC.
 			</h3>
-			{/* sticky nav using hook */}
-			{/* <nav ref={stickyRef} className={('Header-nav', { sticky })}>
-				<a href='../pages/About'>About</a>
-				<a href='../pages/Contact'>Contact</a>
-			</nav> */}
-			{/* nav without useSticky */}
+
 			<nav className='Header-nav'>
 				<a href='../pages/About'>About</a>
 				<a href='../pages/Contact'>Contact</a>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,12 +9,6 @@ export default function Navbar() {
 				src={require('../images/Clay-Beard-1.jpg')}
 				alt='Clay Beard Profile'
 			/>
-			<ul className='Sidebar-menu'>
-				<li className='Sidebar-menu-item'>One</li>
-				<li className='Sidebar-menu-item'>Two</li>
-				<li className='Sidebar-menu-item'>Three</li>
-				<li className='Sidebar-menu-item'>Four</li>
-			</ul>
 		</aside>
 	);
 }
