bb87e798af1c0176bb73cb45619490a85739ffcc
h-elliot <hunter.e.canavan@gmail.com>
2023-07-22 11:08:55 -0400

    navbar added, scrolling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 import '../styles/Navbar.css';
 
 export default function Navbar() {
-	const [open, setOpen] = React.useState(false);
+	// const [open, setOpen] = React.useState(false);
 
-	const handleOpen = () => {
-		setOpen(!open);
+	// const handleOpen = () => {
+	// 	setOpen(!open);
 
-		return (
-			<header className='App-header'>
-				<h3 className='Header-text' id='Header-title'>
-					Beard Mediations, LLC.
-				</h3>
-				<div className='Header-text' id='Header-text-links'>
-					{/* <a href='http://localhost:3000/'>About</a> */}
-					<a href='http://localhost:3000/'>Pricing</a>
-					{/* <a href='http://localhost:3000/'>Credits</a> */}
-				</div>
-			</header>
-		);
-	};
+	return (
+		<header className='App-header'>
+			<h3 className='Header-text' id='Header-title'>
+				Beard Mediations, LLC.
+			</h3>
+			<div className='Header-text' id='Header-text-links'>
+				{/* <a href='http://localhost:3000/'>About</a> */}
+				<a href='http://localhost:3000/'>Pricing</a>
+				{/* <a href='http://localhost:3000/'>Credits</a> */}
+			</div>
+		</header>
+	);
 }
+// }
