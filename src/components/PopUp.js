3895a8a5f87f0e5674b256842790bf953c24a90a
h-elliot <hunter.e.canavan@gmail.com>
2023-08-03 18:04:36 -0400

    card.css mobile version

--- src/components/PopUp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../styles/PopUp.css';
-import Pricing from './Pricing.js';
-
-// shell of pop-op that will house the pricing information
-
-export default function PopUp() {
-	return <Pricing />;
-}
