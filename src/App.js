9c579a82032908919270e124989eebb539783535
h-elliot <hunter.e.canavan@gmail.com>
2023-08-03 16:15:59 -0400

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'react-tabs/style/react-tabs.css';
 import './App.css';
 import Navbar from './components/Navbar';
 import PageContent from './components/PageContent';
 import HeroImg from './components/HeroImg';
 import useSticky from './hooks/useSticky';
-// import Pricing from './components/Pricing';
-// import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 function App() {
 	const { sticky, stickyRef } = useSticky();
-	// const { homePage, setHomePage } = useState(false);
-
-	// function changeHomePage() {
-	// 	setHomePage(!homePage);
-	// }
 
 	return (
 		<div className='App'>
@@ -29,12 +22,7 @@ function App() {
 					<HeroImg />
 				</div>
 				<div className='Page-Content'>
-					{/* first version, static page: */}
 					<PageContent />
-					{/* ========== */}
-					{/* second version, using state: (unexpected outcome, shows Pricing be default?) */}
-					{/* {homePage ? <HomePageContent /> : <Pricing />} */}
-					{/* third version uses Tabs in components */}
 				</div>
 			</main>{' '}
 		</div>
