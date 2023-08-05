a5f476a1d03ef38f1ea965fd87119afdad30bd4e
h-elliot <hunter.e.canavan@gmail.com>
2023-08-05 15:28:13 -0400

    before reboot

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,38 +1,22 @@
 import React from 'react';
 import '../styles/Pricing.css';
-import Card from './Card.js';
 
 export default function Pricing() {
 	return (
 		<main className='Pricing-all'>
 			<h3 className='Pricing-title'>Pricing & Travel Fees</h3>
-			<ul className='Pricing-text'>
+			<p className='Pricing-text'>
 				{' '}
-				{/* long */}
-				<li className='Pricing-text-item'>
-					Full day mediation: (9 AM to 5 PM) $1250 per party
-				</li>
-				<li className='Pricing-text-item'>
-					Half day mediation: (9 AM to 1 PM) $750 per party
-				</li>
-				<li className='Pricing-text-item'>
-					Typically, mediations will be conducted at my Corsicana office
-					(located at 504 West 2nd Ave., Corsicana,TX.)
-				</li>
-				<li className='Pricing-text-item'>
-					I will travel if preferred and/or necessary, with travel rates of
-					$150/hour applicable. No travel charge within a 75 mile radius of
-					Corsicana.
-				</li>
-				<li className='Pricing-text-item'>
-					$300 per part travel charge plus any actual out-of-pocket costs
-					(lodging, plane tickets, etc.) outside 75 mile radius of Corsicana.
-				</li>
-				<li className='Pricing-text-item'>
-					Specialized multi-day mediations for complex cases can be arranged at
-					either the home office or location of your choice.
-				</li>
-			</ul>
+				Full day mediation: (9 AM to 5 PM) $1250 per party Half day mediation:
+				(9 AM to 1 PM) $750 per party Typically, mediations will be conducted at
+				my Corsicana office (located at 504 West 2nd Ave., Corsicana,TX.) I will
+				travel if preferred and/or necessary, with travel rates of $150/hour
+				applicable. No travel charge within a 75 mile radius of Corsicana. $300
+				per part travel charge plus any actual out-of-pocket costs (lodging,
+				plane tickets, etc.) outside 75 mile radius of Corsicana. Specialized
+				multi-day mediations for complex cases can be arranged at either the
+				home office or location of your choice.
+			</p>
 		</main>
 	);
 }
