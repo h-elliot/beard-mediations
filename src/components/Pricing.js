c4b1c41dbb0b56212968820667ab0648c85c129e
h-elliot <hunter.e.canavan@gmail.com>
2023-08-05 17:09:49 -0400

    fix html,body height 100%

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -4,18 +4,30 @@ import '../styles/Pricing.css';
 export default function Pricing() {
 	return (
 		<main className='Pricing-all'>
-			<h3 className='Pricing-title'>Pricing & Travel Fees</h3>
+			<h3 className='Pricing-title' id='Pricing-title-1'>
+				Mediation Pricing
+			</h3>
 			<p className='Pricing-text'>
 				{' '}
-				Full day mediation: (9 AM to 5 PM) $1250 per party Half day mediation:
-				(9 AM to 1 PM) $750 per party Typically, mediations will be conducted at
-				my Corsicana office (located at 504 West 2nd Ave., Corsicana,TX.) I will
-				travel if preferred and/or necessary, with travel rates of $150/hour
-				applicable. No travel charge within a 75 mile radius of Corsicana. $300
-				per part travel charge plus any actual out-of-pocket costs (lodging,
-				plane tickets, etc.) outside 75 mile radius of Corsicana. Specialized
-				multi-day mediations for complex cases can be arranged at either the
-				home office or location of your choice.
+				<strong>Full day mediation: </strong>
+				(9 AM to 5 PM) $1250 per party <br />
+				<strong>Half day mediation: </strong>
+				(9 AM to 1 PM) $750 per party
+				<br />
+				Specialized multi-day mediations for complex cases can be arranged at
+				either the home office or location of your choice.
+			</p>
+			<h3 className='Pricing-title' id='Pricing-title-2'>
+				Travel Fees
+			</h3>
+			<p className='Pricing-text'>
+				{' '}
+				Typically, mediations will be conducted at my Corsicana office (located
+				at 504 West 2nd Ave., Corsicana,TX.) I will travel if preferred and/or
+				necessary, with travel rates of $150/hour applicable. <br />
+				No travel charge within a 75 mile radius of Corsicana. <br />
+				$300 per part travel charge plus any actual out-of-pocket costs
+				(lodging, plane tickets, etc.) outside 75 mile radius of Corsicana.
 			</p>
 		</main>
 	);
