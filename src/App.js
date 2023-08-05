e99e785d0617d495f96037224b929f87fa2ba63f
h-elliot <hunter.e.canavan@gmail.com>
2023-08-05 19:30:43 -0400

    make space for header/footer

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import PageContent from './components/PageContent';
 import HeroImg from './components/HeroImg';
-import useSticky from './hooks/useSticky';
 
 function App() {
-	const { sticky, stickyRef } = useSticky();
-
 	return (
 		<div className='App'>
-			<Navbar />
-			<div
-				style={{
-					height: sticky ? `${stickyRef.current?.clientHeight}px` : '0px',
-				}}
-			/>
 			<main className='Main-container'>
+				<div className='Navbar-wrapper'>
+					<Navbar />
+				</div>
 				<div className='Hero-Img'>
 					<HeroImg />
 				</div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/Navbar.css';
 
 export default function Navbar() {
-	const [open, setOpen] = React.useState(false);
-
-	const handleOpen = () => {
-		setOpen(!open);
-		console.log('setOpen() called');
-
-		return (
-			<div>
-				<header className='App-header'>
-					<h3 className='Header-text' id='Header-title'>
-						Beard Mediations, LLC.
-					</h3>
-					<div className='Header-text' id='Header-text-links'>
-						<h4 onClick={handleOpen}>≡</h4>
-					</div>
-				</header>
-
-				<style jsx>{`
-					// styles inline
-				`}</style>
-			</div>
-		);
-	};
+	return (
+		<div>
+			<header className='Header-all'>
+				<h3 className='Header-text' id='Header-title'>
+					Beard Mediations, LLC.
+				</h3>
+			</header>
+		</div>
+	);
 }
