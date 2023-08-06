72e5bf4ce181be184c6cb8159e76f0b7425f6b0a
h-elliot <hunter.e.canavan@gmail.com>
2023-08-06 15:38:40 -0400

    cleaned up css for new formatting

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
 	return (
 		<div className='About-text'>
 			<h3 className='About-title'>
-				Whether you represent the condemnor or the landowner, I look forward for
+				Whether you represent the condemnor or the landowner, I look forward to
 				the opportunity to help you reach a successful outcome in your case.
 			</h3>
 			<p className='About-p'>
@@ -15,17 +15,19 @@ export default function About() {
 				law firm right out of Baylor Law School in 1989. Litigation has always
 				been my focus. I participated as part of the acclaimed Baylor Mock Trial
 				Competition Team and was honored to receive the Matt Dawson Award for
-				Outstanding Trial Advocacy at Baylor. I “hit the ground running” trying
-				eminent domain cases, and have been a shareholder at Dawson & Sodd for
-				most of my legal career. I firmly believe my extensive trial and
-				mediation experience handling hundreds of eminent domain cases over the
-				past three decades uniquely qualifies me as a mediator specializing in
-				this field. Only an eminent domain lawyer can understand the particular
-				legal issues involved, the nuances of appraisals, and the motivations
-				involved in a condemnation dispute. My philosophy as a trial lawyer has
-				always been that understanding both sides of a case is absolutely
-				essential to a successful outcome – and it is that same philosophy I
-				will bring to the table as a mediator.
+				Outstanding Trial Advocacy at Baylor. <br />
+				<br />I “hit the ground running” trying eminent domain cases, and have
+				been a shareholder at Dawson & Sodd for most of my legal career. I
+				firmly believe my extensive trial and mediation experience handling
+				hundreds of eminent domain cases over the past three decades uniquely
+				qualifies me as a mediator specializing in this field. Only an eminent
+				domain lawyer can understand the particular legal issues involved, the
+				nuances of appraisals, and the motivations involved in a condemnation
+				dispute. <br />
+				<br />
+				My philosophy as a trial lawyer has always been that understanding both
+				sides of a case is absolutely essential to a successful outcome – and it
+				is that same philosophy I will bring to the table as a mediator.
 			</p>
 		</div>
 	);
