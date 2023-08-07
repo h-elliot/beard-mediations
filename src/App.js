3cbcc2f33a7dbecb2439f5a6f7a55306fa2aab24
h-elliot <hunter.e.canavan@gmail.com>
2023-08-06 20:12:23 -0400

    deploy to heroku - favicon2

--- src/App.js
+++ src/App.js
@@ -13,29 +13,7 @@ function App() {
 			<Helmet>
 				<meta charSet='utf-8' />
 				<title>Beard Mediations, LLC.</title>
-				<link rel='icon' href='./images/favicon/favicon.ico' />
-				{/* <!-- third-generation iPad with high-resolution Retina display: --> */}
-				<link
-					rel='apple-touch-icon'
-					sizes='144x144'
-					href='./images/favicon/apple-touch-icon.png'
-				/>
-				{/* <!-- iPhone with high-resolution Retina display: --> */}
-				<link
-					rel='android-chrome-192x192'
-					sizes='192x192'
-					href='./images/favicon/android-chrome-192x192.png'
-				/>
-				{/* <!-- first- and second-generation iPad: --> */}
-				<link
-					rel='android-chrome-512x512'
-					sizes='512x512'
-					href='./images/favicon/android-chrome-512x512.png'
-				/>
-				{/* <!-- non-Retina iPhone, iPod Touch, and Android 2.1+ devices: --> */}
-				<link rel='favicon-16x16' href='./images/favicon/favicon-16x16.png' />
-				{/* <!-- basic favicon --> */}
-				<link rel='favicon-32x32' href='./images/favicon/favicon-32x32.png' />
+				<link rel='icon' href='./favicon.ico' />
 			</Helmet>
 			<main className='Main-container'>
 				<div className='Navbar-wrapper'>
