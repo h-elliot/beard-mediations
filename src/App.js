42451387cc75009e397c4b5778fbc69d06a32eda
h-elliot <hunter.e.canavan@gmail.com>
2023-08-06 20:06:32 -0400

    deploy to heroku - favicon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import 'react-tabs/style/react-tabs.css';
 import './App.css';
 import Navbar from './components/Navbar';
@@ -9,6 +10,33 @@ import Footer from './components/Footer';
 function App() {
 	return (
 		<div className='App'>
+			<Helmet>
+				<meta charSet='utf-8' />
+				<title>Beard Mediations, LLC.</title>
+				<link rel='icon' href='./images/favicon/favicon.ico' />
+				{/* <!-- third-generation iPad with high-resolution Retina display: --> */}
+				<link
+					rel='apple-touch-icon'
+					sizes='144x144'
+					href='./images/favicon/apple-touch-icon.png'
+				/>
+				{/* <!-- iPhone with high-resolution Retina display: --> */}
+				<link
+					rel='android-chrome-192x192'
+					sizes='192x192'
+					href='./images/favicon/android-chrome-192x192.png'
+				/>
+				{/* <!-- first- and second-generation iPad: --> */}
+				<link
+					rel='android-chrome-512x512'
+					sizes='512x512'
+					href='./images/favicon/android-chrome-512x512.png'
+				/>
+				{/* <!-- non-Retina iPhone, iPod Touch, and Android 2.1+ devices: --> */}
+				<link rel='favicon-16x16' href='./images/favicon/favicon-16x16.png' />
+				{/* <!-- basic favicon --> */}
+				<link rel='favicon-32x32' href='./images/favicon/favicon-32x32.png' />
+			</Helmet>
 			<main className='Main-container'>
 				<div className='Navbar-wrapper'>
 					<Navbar />
