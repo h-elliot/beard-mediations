344cb88c0c95448bb668c1ccc3b2fa6dac2181c2
h-elliot <hunter.e.canavan@gmail.com>
2023-08-09 17:48:54 -0400

    -m 'cleaned up unused files'

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../styles/PageContent.css';
 import Card from './Card.js';
-import TabComponent from './TabComponent.js';
+
 import Switch from './Switch.js';
 
 export default function PageContent() {
 	return (
 		<div className='Page-content-all'>
 			<Card />
-			{/* <TabComponent /> */}
 			<Switch />
 		</div>
 	);
